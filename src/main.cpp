8762f18270c2a5cbe12ed930869a52146fcd3ca4
Maksym Honcharenko <mhoncharenko@amcbridge.com>
2020-09-01 23:59:28 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	printf("%d extensions supported", extensionCount);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+}
